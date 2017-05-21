076fe0be3d92570fbbb10ad3a06583184b7beebe
Patrick Foh <patrickfoh@gmail.com>
2017-05-21 10:20:59 +0100

    Added tests

--- test/index.test.js
+++ test/index.test.js
@@ -14,7 +14,7 @@ describe('Jusibe', function() {
     it('Should throw an error if constructor arguments are not provided', function(done) {
         expect(function () {
             new Jusibe();
-        }).to.throw(Error, 'Invalid arguments: publicKey and accessToken not specified')
+        }).to.throw(Error, 'Invalid constructor arguments. publicKey or accessToken is defined')
         done()
     })
 
@@ -26,7 +26,7 @@ describe('Jusibe', function() {
 
     it('To have default options', function (done) {
         let instance = new Jusibe('a', 'b');
-        expect(instance.requestOptions).to.not.be.undefined()
+        expect(instance.options).to.not.be.undefined()
         done()
     })
 })
@@ -45,36 +45,38 @@ describe('Jusibe methods', function() {
         done();
     })
 
-    it('have a getDeliveryStatus method', function (done) {
+    it('have a messageStatus method', function (done) {
         let instance = new Jusibe('a', 'b');
-        expect(instance.getDeliveryStatus).to.be.function()
+        expect(instance.messageStatus).to.be.function()
         done();
     })
 })
 
 describe('sendMessage method', function() {
-    it('should throw an error no payload argument is passed', function (done) {
+    it('should throw an error no params argument is passed', function (done) {
         let instance = new Jusibe('a', 'b');
         
         instance.sendMessage()
-            .then(function() {
+            .then(function(response) {
                 done()
             })
             .catch(function (error) {
-                expect(error).to.an.error(Error);
+                expect(error).to.be.an.error(Error);
                 done()
             })
     })
+})
 
-    it('should throw an error if a payload property is not defined', function (done) {
-        let instance = new Jusibe('a', 'b');
+describe('messageStatus method', function() {
+    it('should throw an error is id is undefined or not a string', function (done) {
+        let instance = new Jusibe('a','b');
 
-        instance.sendMessage({ to: 'a', from: 'v', message: 'v'})
-            .then(function() {
+        instance.messageStatus()
+            .then(function (response) {
                 done()
             })
             .catch(function (error) {
-                expect(error).to.an.error(Error);
+                expect(error).to.be.an.error(Error);
                 done()
             })
     })
